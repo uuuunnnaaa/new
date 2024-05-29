3f4163394a43958e0a57ff66f463fd508588ad25
uuuunnnaaa <wing42854@gmail.com>
2024-05-29 14:54:00 +0800

    idk

--- project/src/app/head/head.component.ts
+++ project/src/app/head/head.component.ts
@@ -1,9 +1,12 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { RouterLink, RouterOutlet } from '@angular/router';
+import { AboutComponent } from '../about/about.component';
 
 @Component({
   selector: 'app-head',
   standalone: true,
-  imports: [],
+  imports: [AboutComponent,CommonModule,RouterLink,RouterOutlet],
   templateUrl: './head.component.html',
   styleUrl: './head.component.css'
 })
